92a5e14828a0a894195599293629890dc676b383
moroz <morozarturv@gmail.com>
2014-06-20 14:49:09 +0300

    main page

--- js/common.js
+++ js/common.js
@@ -7,5 +7,25 @@ head.ready(function() {
 		$(".js-popup").hide();
 	});
 
-	console.log($('body').html());
+	// window scroll
+	$(window).scroll(function(){
+		// chips vegitable
+		var chips_vegitable = $('.js-chips-vegitable');
+		chips_vegitable.each(function(){
+			var scroll_top = $(document).scrollTop(),
+					top = $(this).offset().top,
+					window_height = $(window).height(),
+					wrapper_height = $('.wrapper').height();
+			if (scroll_top >= (top - (window_height/2))) {
+				$(this).addClass('is-visible');
+			};
+			if ((scroll_top + window_height) == wrapper_height) {
+				if ($(this).hasClass('chips__vegitable_potatos-box')) {
+					$(this).addClass('is-visible');
+				};
+			};
+		});
+	});
+	
+
 });
